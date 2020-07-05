ebfc5a3f99382d1d3b0dc2e1d808f71acaced969
Fabien Molinet <fabien.molinet@gmail.com>
2020-07-05 20:08:20 +0200

    move identity server to it's own subdomain: boardz-identity

--- src/frontend/public/global-vars.js
+++ src/frontend/public/global-vars.js
@@ -5,7 +5,7 @@ function getGlobalVars() {
     switch (window.location.host) {
         case "boardz.fabien-molinet.fr":
             return {
-                IdentityServerUri: "https://boardz.fabien-molinet.fr/api/identity-server/",
+                IdentityServerUri: "https://boardz-identity.fabien-molinet.fr",
                 GameServiceUri: "https://boardz.fabien-molinet.fr/api/game-svc/",
             };
         default: // when running locally
