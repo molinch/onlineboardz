0d3484db021187ee765a4413cd38b69f51d95540
Fabien Molinet <fabien.molinet@gmail.com>
2020-07-05 15:14:25 +0200

    global vars should depend on the host it's running from

--- src/frontend/public/global-vars.js
+++ src/frontend/public/global-vars.js
@@ -1,7 +1,14 @@
 // is there a better way?
 // the goal is to have that file modified just before server starts based on some environment variable data
 
-const globalVars = {
-    IdentityServerUri: "https://localhost:5000",
-    GameServiceUri: "https://localhost:5001",
-};
+if (window.location.host === "boardz.fabien-molinet.fr") {
+    const globalVars = {
+        IdentityServerUri: "https://boardz.fabien-molinet.fr/api/identity-server/",
+        GameServiceUri: "https://boardz.fabien-molinet.fr/api/game-svc/",
+    };
+} else {
+    const globalVars = {
+        IdentityServerUri: "https://localhost:5000",
+        GameServiceUri: "https://localhost:5001",
+    };
+}
