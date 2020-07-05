4ad4d1b4293db15f2a5203a5e2eb54e040f3e2e3
Fabien Molinet <fabien.molinet@gmail.com>
2020-07-05 15:32:20 +0200

    fix global vars

--- src/frontend/public/global-vars.js
+++ src/frontend/public/global-vars.js
@@ -1,14 +1,19 @@
 // is there a better way?
 // the goal is to have that file modified just before server starts based on some environment variable data
 
-if (window.location.host === "boardz.fabien-molinet.fr") {
-    const globalVars = {
-        IdentityServerUri: "https://boardz.fabien-molinet.fr/api/identity-server/",
-        GameServiceUri: "https://boardz.fabien-molinet.fr/api/game-svc/",
-    };
-} else {
-    const globalVars = {
-        IdentityServerUri: "https://localhost:5000",
-        GameServiceUri: "https://localhost:5001",
-    };
+function getGlobalVars() {
+    switch (window.location.host) {
+        case "boardz.fabien-molinet.fr":
+            return {
+                IdentityServerUri: "https://boardz.fabien-molinet.fr/api/identity-server/",
+                GameServiceUri: "https://boardz.fabien-molinet.fr/api/game-svc/",
+            };
+        default: // when running locally
+            return {
+                IdentityServerUri: "https://localhost:5000",
+                GameServiceUri: "https://localhost:5001",
+            };
+    }
 }
+
+const globalVars = getGlobalVars()
